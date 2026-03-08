f8de60506600a2a4be2cd7c9d979bcfca10cb70e
subhahaha <103930641+subhahaha@users.noreply.github.com>
2026-03-08 18:33:37 +0545

    file add

---
+++ config.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzjRJl1SdP5EGQYstj2-8_J0Uafon8ttA",
+  authDomain: "portfolio-sm-7a9c0.firebaseapp.com",
+  projectId: "portfolio-sm-7a9c0",
+  storageBucket: "portfolio-sm-7a9c0.firebasestorage.app",
+  messagingSenderId: "894371020362",
+  appId: "1:894371020362:web:01f242ad724451eddfb0b3",
+  measurementId: "G-54HC3VMQH8"
+};
